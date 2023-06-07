bd00099e927a0023c5a9aa20777c21f84aed715f
wangleiz166 <wangleiz166@126.com>
2023-06-07 02:32:03 +0100

    add function

--- staff/urls.py
+++ staff/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.staff_list, name='staff_list'),
+    path('detail/', views.staff_detail, name='staff_detail'),
+    path('add/', views.staff_add, name='staff_add'),
+    path('bulk_add/', views.staff_bulk_add, name='staff_bulk_add'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def staff_list(request):
     return render(request, 'staff_list.html')
+
+def staff_add(request):
+    return render(request, 'staff_add.html')
+
+def staff_bulk_add(request):
+    return render(request, 'staff_bulk_add.html')
+
+def staff_detail(request):
+    return render(request, 'staff_detail.html')
