d1f1b0afb2b765087f48aec49ec524907d3fe644
wangleiz166 <wangleiz166@126.com>
2023-07-18 01:46:19 +0100

    add

--- setting/views.py
+++ setting/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from user.models import Permission
+from django.db import transaction
 
 # Create your views here.
 def list(request):
     return render(request, 'setting_list.html')
 
+
+
 def permission_edit(request):
-    return render(request, 'permission_setting.html')
+    if request.method == 'POST':
+        # Use transactions to guarantee the atomicity of all update operations
+        with transaction.atomic():
+            # Handling each permission
+            for i in range(1, 7):
+                # A position_id of 1 means selected, 0 means unselected.
+                position_id = 1 if f'menu{i}_view' in request.POST else 0
+                
+                # Find records that meet the conditions and update them
+                permission = Permission.objects.get(user_id=0, permission='Manager', menu_id=i)
+                permission.position_id = position_id
+                permission.save()
+
+        # Redirect to the appropriate page
+        return redirect('/setting/')
+
+    permissions = Permission.objects.filter(user_id=0, permission='Manager').values()
+    permissions_dict = {f'menu{p["menu_id"]}_view': p["position_id"] for p in permissions}
+
+    return render(request, 'permission_setting.html', {'permissions': permissions_dict})
 
 def menu_edit(request):
     return render(request, 'menu_edit.html')
--- user/models.py
+++ user/models.py
@@ -1,3 +1,28 @@
+# INSERT INTO "main"."balancer_permission" ("id", "user_id", "position_id", "menu_id", "permission", "create_time", "is_delete") VALUES
+#   -- Manager Permissions
+#   (1, 0, 1, 1, 'Manager', CURRENT_TIMESTAMP, 0),
+#   (2, 0, 1, 2, 'Manager', CURRENT_TIMESTAMP, 0),
+#   (3, 0, 1, 3, 'Manager', CURRENT_TIMESTAMP, 0),
+#   (4, 0, 1, 4, 'Manager', CURRENT_TIMESTAMP, 0),
+#   (5, 0, 1, 5, 'Manager', CURRENT_TIMESTAMP, 0),
+#   (6, 0, 1, 6, 'Manager', CURRENT_TIMESTAMP, 0),
+  
+#   -- Employee Permissions
+#   (7, 0, 1, 1, 'Employee', CURRENT_TIMESTAMP, 0),
+#   (8, 0, 1, 2, 'Employee', CURRENT_TIMESTAMP, 0),
+#   (9, 0, 1, 3, 'Employee', CURRENT_TIMESTAMP, 0),
+#   (10, 0, 1, 4, 'Employee', CURRENT_TIMESTAMP, 0),
+#   (11, 0, 1, 5, 'Employee', CURRENT_TIMESTAMP, 0),
+#   (12, 0, 1, 6, 'Employee', CURRENT_TIMESTAMP, 0),
+  
+#   -- IT Administrator Permissions
+#   (13, 0, 1, 1, 'IT Administrator', CURRENT_TIMESTAMP, 0),
+#   (14, 0, 1, 2, 'IT Administrator', CURRENT_TIMESTAMP, 0),
+#   (15, 0, 1, 3, 'IT Administrator', CURRENT_TIMESTAMP, 0),
+#   (16, 0, 1, 4, 'IT Administrator', CURRENT_TIMESTAMP, 0),
+#   (17, 0, 1, 5, 'IT Administrator', CURRENT_TIMESTAMP, 0),
+#   (18, 0, 1, 6, 'IT Administrator', CURRENT_TIMESTAMP, 0);
+
 from django.db import models
 
 class User(models.Model):
@@ -21,7 +46,7 @@ class Permission(models.Model):
     user_id = models.IntegerField()
     position_id = models.IntegerField()
     menu_id = models.IntegerField()
-    permission = models.IntegerField()
+    permission = models.CharField(max_length=100)
     create_time = models.DateTimeField(auto_now_add=True)
     is_delete = models.BooleanField(default=False)
 
