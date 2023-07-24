dca8f4185aab8b851d65191d2c684e24e3469326
瑞浩 刘 <lrh500@outlook.com>
2023-07-24 01:48:46 +0100

    add download function of  totalwork

--- totalWork/urls.py
+++ totalWork/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('adminRoles/', views.admin_roles, name='admin_roles'),
     path('overallCalcs/', views.overall_calcs, name='overall_calcs'),
     path('frozenModules/', views.frozen_modules, name='frozen_modules'),
+    path('download_total_work/', views.export_total_work_to_csv, name='download_total_work'),
+    path('download_admin_roles/', views.download_admin_roles, name='download_admin_roles'),
+    path('download_overall_calcs/', views.download_overall_calcs, name='download_overall_calcs'),
 ]
